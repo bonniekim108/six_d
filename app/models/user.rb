9af0b2d05a8b1c54a9206260c1983a4ba644805c
Bonnie <bonniekim108@yahoo.com>
2015-02-28 18:06:13 -0800

    Changes to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,15 @@
 class User < ActiveRecord::Base
-
+  before_save { self.email = email.downcase }
   has_secure_password 
-  has_many :sent_invitations
-  belongs_to :invitation
-
   validates :password, length: { minimum: 5 }, allow_nil: true
   validates_presence_of :first_name, :last_name, :username, :email, :password, :password_confirmation
   validates :email, uniqueness: { case_sensitive: false }
   validates :email, presence: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
+
+  has_many :sent_invitations
+  belongs_to :invitation
+  has_many :invitees, through: :friends
+  has_many :friendships
 
   def self.authenticate(login, password)
     user = find_by_login(login)
