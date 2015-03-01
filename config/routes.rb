6254ca4721e02c616a9912e8c1872d68f8d5a315
Bonnie <bonniekim108@yahoo.com>
2015-02-28 18:04:28 -0800

    Changes to routes

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,10 @@ Rails.application.routes.draw do
   get '/signup' => 'users#new'
   post '/users' => 'users#create'
 
+  # GmailAlerts::Application.routes.draw do
+  #   root to: 'sessions#new'
+  #   resources :sessions, only: :index
+  #   get '/auth/:provider/callback' => 'sessions#create'
+  # end
 
 end
