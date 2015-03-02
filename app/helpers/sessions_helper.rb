59cf796f1b2a71d6db56d0c81a370882c9cdc10d
Bonnie <bonniekim108@yahoo.com>
2015-03-02 11:43:17 -0800

    Changes to helper files

---
+++ app/helpers/sessions_helper.rb
@@ -0,0 +1,44 @@
+module SessionsHelper
+
+  # Logs in the user
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+
+  # Returns current logged-in user
+  def current_user
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: session[:user_id])
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(:remember, cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
+    end
+  end
+
+  # Returns true if user is logged in
+  def logged_in?
+    !current_user.nil?
+  end
+
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+
+  # Logs out current user
+  def log_out
+    forget(current_user)
+    session.delete(:user_id)
+    @current_user = nil
+  end
+end
