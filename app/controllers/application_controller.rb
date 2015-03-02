eb191c6a801d1edec73a0abeef093f867b33d4f1
Bonnie <bonniekim108@yahoo.com>
2015-03-02 12:00:13 -0800

    Changes made to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,22 +3,32 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
 
+  private
+
+  def logged_in? 
+    current_user
+  end
+  helper_method :logged_in?
+
   def current_user
-    @current_user ||= User.find_by(id: session[:user])
+    if session[:user_id]
+      @current_user ||= User.find(session[:user_id])
+    elsif cookies.permanent.signed[:remember_me_token]
+      verification = Rails.application.message_verifier(:remember_me).verify(cookies.permanent.signed[:remember_me_token])
+      if verification
+        Rails.logger.info "Logging in through cookie"
+        @current_user ||= User.find(verification)
+      end
+    end
   end
+  helper_method :current_user
 
-  def logged_in?
-    curent_user != nil
+  def require_user
+    if current_user
+      true
+    else
+      redirect_to new_user_session_path, notice: "You must be logged in to access this page."
+    end
   end
-  # before_action :authenticate_user!
-
-  # def index
-  # end
-
-  # def authenticate_user!
-  #   if current_user.nil?
-  #     redirect_to root_path
-  #   end
-  # end
   
 end
