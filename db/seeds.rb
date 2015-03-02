66394d1baad146cec1fe1fd89b45b29f1ea0d93e
Bonnie <bonniekim108@yahoo.com>
2015-03-02 11:32:44 -0800

    Changes to seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,9 +8,10 @@
 
 users = User.create([
 
-  {first_name: "DonnaJo", last_name: "Tanner", username: "fullhouse", email: "djtanner@example.com", password: "abc123", password_confirmation: "abc123"},
-  {first_name: "Doogie", last_name: "Howser", username: "doogiehowser", email: "doogie@example.com", password: "abc123", password_confirmation: "abc123"},
-  {first_name: "Winnie", last_name: "Cooper", username: "wonderyears", email: "kevinandwinnie@example.com", password: "abc123", password_confirmation: "abc123"},
-  {first_name: "Zack", last_name: "Morris", username: "savedbythe", email: "zackmorris@example.com", password: "abc123", password_confirmation: "abc123"} 
+  {first_name: "DonnaJo", last_name: "Tanner", username: "fullhouse", email: "djtanner@example.com", password: "abc123", password_confirmation: "abc123", admin: true, activated: true, activated_at: Time.zone.now},
+  {first_name: "Doogie", last_name: "Howser", username: "doogiehowser", email: "doogie@example.com", password: "abc123", password_confirmation: "abc123", admin: false, activated: true, activated_at: Time.zone.now},
+  {first_name: "Winnie", last_name: "Cooper", username: "wonderyears", email: "kevinandwinnie@example.com", password: "abc123", password_confirmation: "abc123", admin: false, activated: true, activated_at: Time.zone.now},
+  {first_name: "Zack", last_name: "Morris", username: "savedbythe", email: "zackmorris@example.com", password: "abc123", password_confirmation: "abc123", admin: false, activated: true, activated_at: Time.zone.now} 
 
   ])
+
