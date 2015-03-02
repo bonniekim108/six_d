ea7ff4ee5e4c184243c0571928f49a4039858cb6
Bonnie <bonniekim108@yahoo.com>
2015-03-02 12:01:04 -0800

    Changes to create method in sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,19 +4,25 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password)
-      log_in user
-      redirect_to user #redirects to user's profile page upon log in
+    user = User.find_by(email: params[:session][:email])
+    if user && user.authenticate(params[:session][:password])
+      if user.activated? # lets users log in only if they are activated. 
+        log_in user
+        params[:session][:remember_me] == '1'? remember(user) :forget(user)
+        redirect_back_or user
+      else
+        message = "Account not activated."
+        message += "Check email for activation link."
+        flash[:warning] = message
+        redirect_to root_url
+      end
     else
-      flash.now[:danger] = 'Invalid login'
+      flash.now[:danger] = 'There was a problem logging in. Please check your email and password.'
       render 'new'
     end
   end
-
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
   end
-
 end
