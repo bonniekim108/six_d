efc4ec292fd6dedd111343c3b65b154d80391325
Bonnie <bonniekim108@yahoo.com>
2015-03-02 11:50:30 -0800

    Previews for activemailer

--- spec/mailers/previews/user_mailer_preview.rb
+++ spec/mailers/previews/user_mailer_preview.rb
@@ -1,4 +1,17 @@
 # Preview all emails at http://localhost:3000/rails/mailers/user_mailer
 class UserMailerPreview < ActionMailer::Preview
 
+  # Preview this email at
+  # http://localhost:3000/rails/mailers/user_mailer/welcome_email
+  def welcome_email
+    user = User.first
+    user.activation_token = User.new_token
+    UserMailer.welcome_email(user)
+  end
+
+  # Preview this email at
+  # http://localhost:3000/rails/mailers/user_mailer/password_reset
+  def password_reset
+    UserMailer.password_reset
+  end
 end
