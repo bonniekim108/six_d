f0ba1f6b6168485e08a547c24182ff7c10b8502c
Bonnie <bonniekim108@yahoo.com>
2015-03-02 11:53:40 -0800

    Deleted email with name in usermailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,8 +3,15 @@ class UserMailer < ApplicationMailer
 
   def welcome_email(user)
     @user = user
-    email_with_name = %("#{@user.name}" <#{@user.email}>)
+    # email_with_name = %("#{@user.name}" <#{@user.email}>)
     mail(to: @user.email, subject: "Welcome to Six Degrees")
   end
+
+  def invitation(invitee)
+    @invitee = invitee
+    # email_with_name = %("#{@invitee.name}" <#{@invitee.email}>)
+    mail(to: @invitee.email, subject: "Invited to Join Six Degrees")
+  end
+
 
 end
