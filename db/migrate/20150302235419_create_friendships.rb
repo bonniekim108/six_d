82514e063bdf66df9b638328f191d53a14c417dc
Bonnie <bonniekim108@yahoo.com>
2015-03-03 09:44:09 -0800

    Changes to migrations file

--- db/migrate/20150302235419_create_friendships.rb
+++ db/migrate/20150302235419_create_friendships.rb
@@ -1,8 +1,11 @@
 class CreateFriendships < ActiveRecord::Migration
   def change
     create_table :friendships do |t|
-
-      t.timestamps null: false
+      t.integer :user_id
+      t.integer :invitee_id
+      t.timestamps
     end
+
+    add_index :friendships, [:user_id, :invitee_id]
   end
 end
