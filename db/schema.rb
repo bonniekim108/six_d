cc42b3931935fa3470631d2892da19ebbd191288
gogojojo <joseph.gochua@gmail.com>
2015-03-03 10:55:17 -0800

    added form for msgsm updated user controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150303014548) do
+ActiveRecord::Schema.define(version: 20150303184307) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,17 @@ ActiveRecord::Schema.define(version: 20150303014548) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "messages", force: :cascade do |t|
+    t.string   "subject"
+    t.string   "body"
+    t.string   "sender"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "messages", ["user_id"], name: "index_messages_on_user_id", using: :btree
+
   create_table "notifications", force: :cascade do |t|
     t.text     "content"
     t.integer  "user_id"
@@ -66,5 +77,6 @@ ActiveRecord::Schema.define(version: 20150303014548) do
     t.datetime "activated_at"
   end
 
+  add_foreign_key "messages", "users"
   add_foreign_key "notifications", "users"
 end
