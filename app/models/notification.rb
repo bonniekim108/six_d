15f3ec195973d5e9f6b6976d228f0ea1ac2a6c72
Bonnie <bonniekim108@yahoo.com>
2015-03-04 00:52:12 -0800

    changes made for adding images to notifications page

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,10 +1,13 @@
 class Notification < ActiveRecord::Base
+  attr_accessible :content, :user_id, :document_attributes # this part allows us to say status.document
   belongs_to :user
+  belongs_to :document
+  accepts_nested_attributes_for :document
   default_scope -> { order(created_at: :desc) } # ensures that most recent notification is first 
   mount_uploader :picture, PictureUploader
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
-  validate :picture_size
+  validate :picture_size # custom validation so use validate instead of validation
 
   private
 
