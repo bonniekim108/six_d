90c4db6186430ec0ad82f2e40930c814587894ca
Bonnie <bonniekim108@yahoo.com>
2015-03-05 06:09:16 -0800

    Mostly styling to landing page including carousel added navigation bar changed colors changed

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,7 +15,6 @@ class SessionsController < ApplicationController
         #message = "Account not activated. Check email for activation link."
         #flash[:warning] = message
         #redirect_to root_url
-      #end
     else
       # flash.now[:danger] = 'Login error - please check your email and password.'
       render 'new'
