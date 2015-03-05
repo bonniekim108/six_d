f9c809690ff393b4c9dc875c6afa3369ab714855
Bonnie <bonniekim108@yahoo.com>
2015-03-04 19:50:45 -0800

    more heroku testing

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -4,7 +4,6 @@ class PictureUploader < CarrierWave::Uploader::Base
 
 
   # Include RMagick or MiniMagick support:
-  include CarrierWave::RMagick
   include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
