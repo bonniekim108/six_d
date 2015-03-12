d03c335fac44f62bd6410d2f6d1794a0cb3ff3d5
Bonnie <bonniekim108@yahoo.com>
2015-03-12 16:05:25 -0700

    Changes to user authentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,24 +4,18 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(username: params[:user][:username])
-    if user && user.authenticate(params[:user][:password])
+    user = User.find_by_email(params[:email])
+    if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       redirect_to root_path
-      #if user.activated? # lets users log in only if they are activated. 
-        #log_in user
-        #params[:session][:remember_me] == '1'? remember(user) :forget(user)
-        #redirect_to profile_path
-        #message = "Account not activated. Check email for activation link."
-        #flash[:warning] = message
-        #redirect_to root_url
+   
     else
       # flash.now[:danger] = 'Login error - please check your email and password.'
       render 'new'
     end
   end
   def destroy
-    log_out if logged_in?
-    redirect_to root_url
+    session[:user_id] = nil
+    redirect_to '/login'
   end
 end
