91ecb5634cf80a38361d8b90106b89df6ea5bb5e
Bonnie <bonniekim108@yahoo.com>
2015-03-17 10:03:04 -0700

    Commented out notifications for the present

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,37 +1,41 @@
-class NotificationsController < ApplicationController
+# class NotificationsController < ApplicationController
 
-  before_action :logged_in_user, only: [:create, :destroy]
-  before_action :correct_user, only: :destroy
+#   before_action :logged_in_user, only: [:create, :destroy]
+#   before_action :correct_user, only: :destroy
 
-  def create
-    @notification = current_user.notifications.build(notification_params)
-    if @notification.save
-      flash[:success] = "Notification created! Great!"
-      redirect_to root_url
-    else
-      @feed_items = []
-      render 'static_pages/home'
-    end
-  end
+#   def index
+#     @notifications = current_user.notifications
 
-  def destroy
-    @notification.destroy
-    flash[:success] = "Notification deleted"
-    redirect_to request.referrer || root_url
-    # request.referrer redirects back to page giving delete request both cases
-  end
+#   def new
+#     @notification = Notification.new
 
-  private
+#   def create
+#     @notification = Notification.new(params.require(:notification).permit(:body))
+#     if @notification.save
+#       flash[:success] = "A new notification has been created"
+#     else
+#       render :new
+#     end
+#   end
 
-    def notification_params
-      params.require(:notification).permit(:content, :picture)
-    end
+#   def destroy
+#     @notification.destroy
+#     flash[:success] = "Notification deleted"
+#     redirect_to request.referrer || root_url
+#     # request.referrer redirects back to page giving delete request both cases
+#   end
 
-    def correct_user
-      @notification = current_user.notifications.find_by(id: params[:id])
-      redirect_to root_url if @notification.nil?
-    end
-end
+#   private
+
+#     def notification_params
+#       params.require(:notification).permit(:content, :picture)
+#     end
+
+#     def correct_user
+#       @notification = current_user.notifications.find_by(id: params[:id])
+#       redirect_to root_url if @notification.nil?
+#     end
+# end
 
 
 
--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,19 +1,22 @@
 class Notification < ActiveRecord::Base
-  belongs_to :user
+
   #belongs_to :document
   #accepts_nested_attributes_for :document
+  # mount_uploader :picture, PictureUploader
+
+  belongs_to :user
   default_scope -> { order(created_at: :desc) } # ensures that most recent notification is first 
-  mount_uploader :picture, PictureUploader
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
   validate :picture_size # custom validation so use validate instead of validation
+  field :body
 
   private
 
-    # Validates size of uploaded picture
-    def picture_size
-      if picture.size > 5.megabytes
-        errors.add(:picture, "should be less than 5MB")
-      end
-    end
-end
+#     # Validates size of uploaded picture
+#     def picture_size
+#       if picture.size > 5.megabytes
+#         errors.add(:picture, "should be less than 5MB")
+#       end
+#     end
+# end
