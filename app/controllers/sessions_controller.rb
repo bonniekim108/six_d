dc174ab2d0442e69500f5221b69d670ff48851aa
Bonnie <bonniekim108@yahoo.com>
2015-03-17 07:26:26 -0700

    Added flash message for unsuccessful login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,13 +5,15 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by_email(params[:email])
+    # if user exists and password is correct
     if user && user.authenticate(params[:password])
+      #save user id inside browser cookie. This keeps user logged in when they navigate around website
       session[:user_id] = user.id
       redirect_to root_path
    
     else
-      # flash.now[:danger] = 'Login error - please check your email and password.'
-      render 'new'
+      flash[:danger] = "Incorrect username and/ or password"
+      redirect_to '/login'
     end
   end
   def destroy
